770d46cc63e838e8fea58af276b2ab4758a4731e
m-ilnitsky <ilnitsky@ngs.ru>
2019-05-16 19:36:14 +0700

    Task02_MultiplicationTable: исправлено по аналогии с первой задачей

--- Task02_MultiplicationTable/MultiplicationTable.cs
+++ Task02_MultiplicationTable/MultiplicationTable.cs
@@ -10,25 +10,25 @@ namespace Task02_MultiplicationTable
     {
         static void Main(string[] args)
         {
-            uint min = 2;
-            uint max = 28;
-            Console.Write("Введите максимальное число таблицы умножения (от {0} до {1}): ", min, max);
-            uint maxNumber = Convert.ToUInt32(Console.ReadLine());
-            if (maxNumber < min)
+            const int MIN = 2;
+            const int MAX = 28;
+            Console.Write("Введите максимальное число таблицы умножения (от {0} до {1}): ", MIN, MAX);
+            int maxNumber = Convert.ToInt32(Console.ReadLine());
+            if (maxNumber < MIN)
             {
-                Console.WriteLine("ОШИБКА: Число меньше {0}! Поэтому бдем считать, что оно равно {0}", min);
-                maxNumber = min;
+                Console.WriteLine("ОШИБКА: Число меньше {0}! Поэтому будем считать, что оно равно {0}", MIN);
+                maxNumber = MIN;
             }
-            else if (maxNumber > max)
+            else if (maxNumber > MAX)
             {
-                Console.WriteLine("ОШИБКА: Число больше {0}! Поэтому бдем считать, что оно равно {0}", max);
-                maxNumber = max;
+                Console.WriteLine("ОШИБКА: Число больше {0}! Поэтому будем считать, что оно равно {0}", MAX);
+                maxNumber = MAX;
             }
 
-            uint lineLength = 0;
-            for (uint j = 1; j <= maxNumber; j++)
+            int lineLength = 0;
+            for (int j = 1; j <= maxNumber; j++)
             {
-                uint maxLength = (uint)Convert.ToString(maxNumber * j).Length;
+                int maxLength = Convert.ToString(maxNumber * j).Length;
                 lineLength += maxLength + 1;
 
                 if (j == 1)
@@ -41,21 +41,21 @@ namespace Task02_MultiplicationTable
             Console.WriteLine();
 
             StringBuilder sb = new StringBuilder();
-            for (uint i = 0; i < lineLength; i++)
+            for (int i = 0; i < lineLength; i++)
             {
                 sb.Append("-");
             }
 
-            uint prefixLength = (uint)Convert.ToString(maxNumber).Length;
+            int prefixLength = Convert.ToString(maxNumber).Length;
             Console.Write(" {0," + prefixLength + "} +", "");
             Console.Write(sb.ToString());
             Console.WriteLine("-+");
 
-            for (uint i = 1; i <= maxNumber; i++)
+            for (int i = 1; i <= maxNumber; i++)
             {
-                for (uint j = 1; j <= maxNumber; j++)
+                for (int j = 1; j <= maxNumber; j++)
                 {
-                    uint maxLength = (uint)Convert.ToString(maxNumber * j).Length;
+                    int maxLength = Convert.ToString(maxNumber * j).Length;
 
                     if (j == 1)
                     {
