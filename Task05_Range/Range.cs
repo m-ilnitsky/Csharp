e325dc0f9bf1f363d03d153b0b999792c25a1ed3
m-ilnitsky <ilnitsky@ngs.ru>
2019-05-17 16:14:29 +0700

    Task05_Range: реализовал

---
+++ Task05_Range/Range.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task05_Range
+{
+    class Range
+    {
+        private double from;
+        private double to;
+
+        public Range(double from, double to)
+        {
+            if (from <= to)
+            {
+                this.from = from;
+                this.to = to;
+            }
+            else
+            {
+                this.from = to;
+                this.to = from;
+            }
+
+        }
+
+        public double From
+        {
+            get { return from; }
+            set { from = value; }
+        }
+
+        public double To
+        {
+            get { return to; }
+            set { to = value; }
+        }
+
+        public double getLength()
+        {
+            return Math.Abs(to - from);
+        }
+
+        public bool IsInside(double point)
+        {
+            return (from <= point) && (point <= to);
+        }
+
+        public override string ToString()
+        {
+            return "[" + from + " ; " + to + "]";
+        }
+    }
+}
---
+++ Task05_Range/RangeTester.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task05_Range
+{
+    class RangeTester
+    {
+        private static void TestPoint(Range range, double point)
+        {
+            if (range.IsInside(point))
+            {
+                Console.WriteLine("Точка {0} принадлежит диапазону {1}.", point, range);
+            }
+            else
+            {
+                Console.WriteLine("Точка {0} НЕ принадлежит диапазону {1}.", point, range);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Range range1 = new Range(1, 15);
+            Range range2 = new Range(-1, -15);
+            Range range3 = new Range(15.7, -15.7);
+
+            Console.WriteLine("Диапазон №1 : {0}", range1);
+            Console.WriteLine("Диапазон №2 : {0}", range2);
+            Console.WriteLine("Диапазон №3 : {0}", range3);
+            Console.WriteLine();
+
+            double[] points = { -15.7, -15, -1, 0, 1, 15, 15.7 };
+
+            foreach (double point in points)
+            {
+                TestPoint(range1, point);
+                TestPoint(range2, point);
+                TestPoint(range3, point);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
