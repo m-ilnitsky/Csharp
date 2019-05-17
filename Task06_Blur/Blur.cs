0acab2cfd8419b2782c95cc2dbd533dc69846402
m-ilnitsky <ilnitsky@ngs.ru>
2019-05-17 17:24:27 +0700

    Task06_Blur: заготовка

---
+++ Task06_Blur/Blur.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Task06_Blur
+{
+    class Blur
+    {
+        static void Main(string[] args)
+        {
+            const int maxRgb = 255;
+            Bitmap image = new Bitmap("..\\..\\image.jpg");
+
+            for (int y = 0; y < image.Height; ++y)
+            {
+                for (int x = 0; x < image.Width; ++x)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(maxRgb - pixel.R, maxRgb - pixel.G, maxRgb - pixel.B);
+
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+
+            image.Save("out.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
